69ece5e3ebd50bfc9a8f17c44a97ddb81bf8078c
Cristian Maximiliano Orellana <167025087+MaximilianoOrellana02@users.noreply.github.com>
2026-06-30 15:41:43 -0300

    Refactor CORS setup to use allowed domains
    Updated CORS configuration to allow specific origins and handle requests without origin.

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,27 @@ import cors from "cors";
 import sequelize from "./config/db";
 import './models/index'
 
+const dominiosPermitidos = [
+  'http://localhost:4200',                  // Tu entorno local
+  'https://canchaya-front.vercel.app'       // Tu frontend en producción
+];
+
 const app: Application = express();
 import rutasPrincipales from './routes/index'
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors({
+  origin: function (origin, callback) {
+    // Permitimos peticiones sin origen (como herramientas tipo Postman) 
+    // o las que estén en nuestra lista de permitidos
+    if (!origin || dominiosPermitidos.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Bloqueado por CORS'));
+    }
+  },
+  credentials: true // Importante si después manejás cookies o tokens JWT
+}));
 app.set("port", process.env.PORT || 3000);
 
 //Rutas
